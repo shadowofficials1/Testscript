6cbb9d770150c9e507105c645027324cdb630117
shadowofficials1 <shadowofficials100@gmail.com>
2025-04-04 13:48:02 +0800

    Create GameLists.lua

---
+++ GameLists.lua
@@ -0,0 +1,8 @@
+-- A table that associates Game Place IDs with their corresponding script URLs
+local Games = {
+    -- Blox Fruits Sea 1, 2, and 3
+    [2753915549] = "https://raw.githubusercontent.com/YourRepo/BloxFruit.lua",   
+}
+
+-- Return the games table to be used in KeySystem.lua
+return Games
