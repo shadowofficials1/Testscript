3ab0672bdaf228796268cdd72aa7edae5f495c48
shadowofficials1 <shadowofficials100@gmail.com>
2025-04-04 13:52:17 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,5 @@
 -- Load the key system first and validate the key
-local KeySystem = loadstring(game:HttpGet("https://raw.githubusercontent.com/YourRepo/KeySystem.lua"))()
+local KeySystem = loadstring(game:HttpGet("https://raw.githubusercontent.com/shadowofficials1/Testscript/refs/heads/main/Keysystem.lua"))()
 
 -- Check if the key was valid
 if KeySystem() then
